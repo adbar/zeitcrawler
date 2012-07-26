4f8743c5393dd913d7099316ed6c68a3305835cb
adrien.barbaresi <adrien.barbaresi@gmail.com>
2012-07-26 17:04:53 +0000

    Fixing errors in CWB-conversion files

--- CWB-XML_join.pl
+++ CWB-XML_join.pl
@@ -1,4 +1,13 @@
 #!/usr/bin/perl
+
+
+###	This script is part of the zeitcrawler (http://code.google.com/p/zeitcrawler/).
+###	It is brought to you by Adrien Barbaresi.
+###	It is freely available under the GNU GPL v3 license (http://www.gnu.org/licenses/gpl.html).
+
+###	WORK IN PROGRESS !
+
+
 use strict;
 use warnings;
 
--- CWB-XML_tag+join.sh
+++ CWB-XML_tag+join.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ABBR_LIST=~/Desktop/DArbeit/korpora/Zprog/share/abbrev.lex
-TOKDIP=~/Desktop/DArbeit/korpora/Zprog/tokenize-DIPPER.pl
+###	This script is part of the zeitcrawler (http://code.google.com/p/zeitcrawler/).
+###	It is brought to you by Adrien Barbaresi.
+###	It is freely available under the GNU GPL v3 license (http://www.gnu.org/licenses/gpl.html).
+
+###	WORK IN PROGRESS !
+
+
 TAGGER=~/Desktop/DArbeit/werkzeuge/TreeTagger/bin/tree-tagger
 OPTIONS="-token -lemma -no-unknown"
 PARMFILE=~/Desktop/DArbeit/werkzeuge/TreeTagger/lib/german-utf8.par
@@ -9,15 +14,7 @@ PARMFILE=~/Desktop/DArbeit/werkzeuge/TreeTagger/lib/german-utf8.par
 
 perl FU-doc-trenn.pl
 
-#read -p "DOCNR ? -> " DOCNR
-#for f in docs/text-*
-#for i in {$START..$DOCNR}
-
-perl $TOKDIP -abbrev $ABBR_LIST docs/text temp
-< docs/temp perl -pe 's/\n/\nSPSPSP\n.\n/g' > temp1
-< temp1 sed -e 's/[„“”‚’]/\n"\n/g' -e 's/\([A-Za-zÄÖÜäöüß]\)\([,;:\.!?"]\)/\1 \2/g' -e 's/\n/\nSPSPSP\n.\n/g' -e 's/ /\n/g' -e '/^$/d' | $TAGGER $OPTIONS $PARMFILE > docs/tags
-rm docs/temp
-#-e 's/„/\n"\n/g' -e 's/“/\n"\n/g' -e 's/&amp;/&/g'
+< docs/text sed -e 's/[„“”‚’]/\n"\n/g' -e 's/\([«»]\)/\n\1\n/g' -e 's/\([A-Za-zÄÖÜäöüß]\)\([,;:\.!?"]\)/\1 \2/g' -e 's/ /\n/g' -e '/^$/d' | $TAGGER $OPTIONS $PARMFILE > docs/tags
 
 perl join.pl
 
